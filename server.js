3b6baf8a3344c2a78103726d465cf316dac852ba
Unknown <oreoboi@gmail.com>
2018-03-29 23:10:08 -0400

    defined frontend route

--- server.js
+++ server.js
@@ -77,6 +77,10 @@ app.use(methodOverride());
         });
     });
 
+     // application -------------------------------------------------------------
+     app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
 
 // listen (start app with node server.js) 
 app.listen(8080);
